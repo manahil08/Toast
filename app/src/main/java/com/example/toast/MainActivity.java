ed657c55c8aec45d02e73769479cc6c507b8f8a2
manahil <bitf18a507@pucit.edu.pk>
2021-12-12 21:42:25 +0500

    second

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast toast = Toast.makeText(this, "Toast Text",
-                Toast.LENGTH_LONG);
-        toast.show();
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View myLayout=layoutInflater.inflate(R.layout.my_layout,(ViewGroup)findViewById(R.id.dialog_container) );
+        ImageView cameraIamge=myLayout.findViewById(R.id.imageViewCamera);
+        cameraIamge.setImageResource(R.drawable.ic_baseline_local_see_24);
+        TextView myMessage=(TextView)myLayout.findViewById(R.id.textViewCameraInfo);
+        myMessage.setText("My Custom Toast");
+        Toast myToast=new Toast(getApplicationContext());
+        myToast.setDuration(Toast.LENGTH_LONG);
+        myToast.setView(myLayout);
+        myToast.show();
+
+
 
     }
 }
