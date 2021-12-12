4af34ab75607cdb42505b2d73c1acc7964879319
manahil <bitf18a507@pucit.edu.pk>
2021-12-12 20:52:14 +0500

    first

---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(this, "Toast Text",
+                Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+}
